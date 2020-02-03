39ddec3e7e9e61654eab8551fc2f150cd1905e8d
baardl <bard.lind@gmail.com>
2020-02-03 18:47:27 +0100

    Read DEVICE_CONNECTION_STRING

--- src/main/java/no/entra/iot/hostobserver/AgentDaemon.java
+++ src/main/java/no/entra/iot/hostobserver/AgentDaemon.java
@@ -8,20 +8,28 @@ import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import static no.entra.iot.hostobserver.utils.PropertyReader.findProperty;
+import static no.entra.iot.hostobserver.utils.PropertyReader.isEmpty;
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class AgentDaemon {
     private static final Logger log = getLogger(AgentDaemon.class);
     public static final String NAME = "iot-edge-host-observer";
+    public static final String DEVICE_CONNECTION_STRING = "DEVICE_CONNECTION_STRING";
 
     //TODO send to Azure IoT
     public static void main(String[] args) {
         log.info("Starting " + NAME);
         AgentDaemon agentDaemon = new AgentDaemon();
-        agentDaemon.reportIpAdressesEveryNSeconds(10);
+        String iotHubDeviceConnectionString = findConnectionString(args);
+        if (isEmpty(iotHubDeviceConnectionString)) {
+            log.error("Missing required property: DEVICE_CONNECTION_STRING, exiting ");
+        } else {
+            agentDaemon.reportIpAdressesEveryNSeconds(10);
+        }
     }
 
-    private void reportIpAdressesEveryNSeconds(int seconds) {
+    void reportIpAdressesEveryNSeconds(int seconds) {
         Timer t = new Timer();
         ReportIpAddresses mTask = new ReportIpAddresses();
 
@@ -31,6 +39,16 @@ public class AgentDaemon {
                 log.info("Shutting down " + NAME);
             }
         });
+    }
+
+    static String findConnectionString(String[] args) {
+        String deviceConnectionString = findProperty(DEVICE_CONNECTION_STRING);
+        if (deviceConnectionString == null) {
+            if (args.length > 0) {
+                deviceConnectionString = args[0];
+            }
+        }
+        return deviceConnectionString;
     }
 
 
