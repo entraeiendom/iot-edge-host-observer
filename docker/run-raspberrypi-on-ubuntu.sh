13504ca4a9e4b5fb5f3606e844119dec663d6151
baardl <bard.lind@gmail.com>
2020-02-03 16:23:46 +0100

    Setup Maven and Docker

--- docker/run-raspberrypi-on-ubuntu.sh
+++ docker/run-raspberrypi-on-ubuntu.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 DOCKER_USER=${1:-.}
-TAG=${2:-rasberypi4}
+TAG=${2:-raspberrypi4}
 CONNECTION_STRING=${3:-connection-to-eg-azure-iot}
 docker run -e DEVICE_CONNECTION_STRING=$CONNECTION_STRING --name=iot-edge-host-observer $DOCKER_USER/iot-edge-host-observer:$TAG
