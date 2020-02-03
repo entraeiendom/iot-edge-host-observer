5d23edc2de60a683be6eff6245026fa4efec1f72
baardl <bard.lind@gmail.com>
2020-02-03 16:29:25 +0100

    Setup Maven and Docker

--- docker/run.sh
+++ docker/run.sh
@@ -2,5 +2,5 @@
 DOCKER_USER=${1:-.}
 TAG=${2:-alpine}
 CONNECTION_STRING=${3:-connection-to-eg-azure-iot}
-echo docker run --name=iot-edge-host-observer $DOCKER_USER/iot-edge-host-observer:$TAG
-docker run --name=iot-edge-host-observer $DOCKER_USER/iot-edge-host-observer:$TAG
+echo docker run -e DEVICE_CONNECTION_STRING=$CONNECTION_STRING --name=iot-edge-host-observer $DOCKER_USER/iot-edge-host-observer:$TAG
+docker run -e DEVICE_CONNECTION_STRING=$CONNECTION_STRING --name=iot-edge-host-observer $DOCKER_USER/iot-edge-host-observer:$TAG
