432d47d5827cc62d675d60b0b9e44fa179a43707
baardl <bard.lind@gmail.com>
2020-02-03 18:39:29 +0100

    Keep running

--- src/main/java/no/entra/iot/hostobserver/AgentDaemon.java
+++ src/main/java/no/entra/iot/hostobserver/AgentDaemon.java
@@ -5,20 +5,53 @@ import org.slf4j.Logger;
 
 import java.net.InetAddress;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class AgentDaemon {
     private static final Logger log = getLogger(AgentDaemon.class);
+    public static final String NAME = "iot-edge-host-observer";
+
+    //TODO send to Azure IoT
     public static void main(String[] args) {
-        Map<String, InetAddress> networkCardConifg = NetworkConfig.findPublicIpAddresses();
-        if (networkCardConifg != null) {
-            for (String cardName : networkCardConifg.keySet()) {
-                InetAddress inetAddress = networkCardConifg.get(cardName);
-                if (inetAddress != null) {
-                    log.info("CardName: {}, Ip: {}", cardName, inetAddress.getHostAddress());
+        log.info("Starting " + NAME);
+        AgentDaemon agentDaemon = new AgentDaemon();
+        agentDaemon.reportIpAdressesEveryNSeconds(10);
+    }
+
+    private void reportIpAdressesEveryNSeconds(int seconds) {
+        Timer t = new Timer();
+        ReportIpAddresses mTask = new ReportIpAddresses();
+
+        t.scheduleAtFixedRate(mTask, 0, seconds * 1000);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                log.info("Shutting down " + NAME);
+            }
+        });
+    }
+
+
+    class ReportIpAddresses extends TimerTask {
+
+        public ReportIpAddresses() {
+            //Some stuffs
+        }
+
+        @Override
+        public void run() {
+            Map<String, InetAddress> networkCardConifg = NetworkConfig.findPublicIpAddresses();
+            if (networkCardConifg != null) {
+                for (String cardName : networkCardConifg.keySet()) {
+                    InetAddress inetAddress = networkCardConifg.get(cardName);
+                    if (inetAddress != null) {
+                        log.info("CardName: {}, Ip: {}", cardName, inetAddress.getHostAddress());
+                    }
                 }
             }
         }
+
     }
 }
